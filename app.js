dd4aae489d97ef3af37a52526ecf0334c303a90d
Rossibelas <guilhermerossi103@gmail.com>
2023-11-17 22:23:37 -0300

    Aula do Guto no Console
    17 11 23

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-function VerificarResposta(){
-    let respostaCerta 
-    respostaCerta = document.getElementById("alternativacerta").checked
-
-    console.log(respostaCerta)
-
-    if(respostaCerta == true){
-        alert("Parabéns!!!\nVocê Acertou.")
-    }else{
-        alert("Que pena, não foi dessa vez.")
-    }
+function escreveTexto(){
+    document.getElementById("titulo").innerText = "Curso de Javascrpt"
+    document.getElementById("texto").innerText = "Estou estudando javascript e espero aprender, ou pelo menos o guto espera que eu aprenda."
 }
+function escreveOi(mensagem){
+    document.write(mensagem)
+}
+ 
+function corDoFundo(cor){
+  document.body.style.backgroundColor=cor
+}
+ 
+function corDoFundo2(){
+    document.body.style.backgroundColor="orange"
+}
+ 
+function corDoFundo3(){
+    document.body.style.backgroundColor="red"
+}
+ 
+// escreveTexto()
+// corDoFundo("blue")
 
+// escreveOi("Teste1<br>")
+// escreveOi("<strong>Mais um teste</strong>")
+// escreveOi("<br>esse eh o ultimo")
+ 
